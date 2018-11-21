6402d762ffc4a963cda32125abedbc60efcc71d1
QiusenHuangGT <qiusenhuang17@hotmail.com>
2018-11-21 16:22:44 -0500

    complete model, update admin

--- DonaTracker_web/Main/admin.py
+++ DonaTracker_web/Main/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from Main.models import UserProfile, Location
+from Main.models import UserProfile, Location, Donation
 # Register your models here.
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'user_type',)
 
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('uuid', 'name', 'type',)
+    list_display = ('uuid', 'name', 'type', 'lat', 'lng')
+
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'short_description', 'value', 'location', 'catgory')
 
 admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Location, LocationAdmin)
+admin.site.register(Donation, DonationAdmin)
--- DonaTracker_web/Main/models.py
+++ DonaTracker_web/Main/models.py
@@ -4,7 +4,7 @@ import uuid
 
 #user
 class UserProfile(models.Model):
-    REGULAR_USER = 'Uset'
+    REGULAR_USER = 'User'
     LOCATION_EMPLOYEE = 'Location Employee'
     MANAGER = 'Manager'
     ADMIN = 'Admin'
@@ -28,6 +28,32 @@ class Location(models.Model):
     uuid = models.UUIDField(primary_key=True, 
         default=uuid.uuid4, 
         editable = False)
-    name = models.CharField(max_length = 128, default = "name undefined")
+    name = models.CharField(max_length = 128, default = "(name)")
+    lat = models.DecimalField(max_digits= 20, decimal_places=17, default = 0.0)
+    lng = models.DecimalField(max_digits= 20, decimal_places=17, default = 0.0)
+    street = models.CharField(max_length = 128, default = "(street)")
+    address = models.CharField(max_length = 128, default = "(address")
+    city = models.CharField(max_length = 20, default = "(city)")
+    State = models.CharField(max_length = 10, default = "(state)")
+    zip = models.IntegerField(default=99999)
     type = models.CharField(max_length = 64, default = "default type")
+    phone = models.IntegerField(default=0)
+    website = models.CharField(max_length = 128, default = "(website)")
+    employees = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    uuid = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    date = models.DateField(auto_now_add=True, editable = False)
+    location = models.ForeignKey(Location)
+    short_description = models.CharField(max_length = 20, default = "short description")
+    full_description = models.CharField(max_length = 256, default = "full description")
+    value = models.DecimalField(max_digits = 10, decimal_places = 2)
+    category = models.CharField(max_length = 128, default = "default")
     
+    def __str__(self):
+        return self.short_description
+
